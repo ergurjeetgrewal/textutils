48d66d52f5d420d6a45ee580c7d80f6d4212add6
Gurjeet Singh Grewal <gurgrewal143@gmail.com>
2021-08-24 16:33:45 +0530

    till video 16

--- src/App.js
+++ src/App.js
@@ -2,44 +2,57 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/textform';
-import {useState} from 'react'
+import React, { useState } from 'react'
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState("light");//whether dark mode is enabled or not
   const [alert, setAlert] = useState(null)
 
-  const setalert = (message, type) =>{
+  const setalert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setAlert(null)
     }, 1500);
   }
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setmode('dark');
       document.body.style.backgroundColor = '#26273a';
-      setalert('Dark Mode has been enabled','success');
+      setalert('Dark Mode has been enabled', 'success');
       document.title = "Textutils Home - darkmode"
     }
-    else{
+    else {
       setmode('light');
       document.body.style.backgroundColor = 'white';
-      setalert('Light Mode has been enabled','success');
+      setalert('Light Mode has been enabled', 'success');
       document.title = "Textutils Home"
     }
   }
   return (
+    <Router>
     <>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-    <TextForm heading = "Enter the text below to analyze" setalert={setalert} mode={mode} />
-    {/* <About/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <TextForm heading="Enter the text below to analyze" setalert={setalert} mode={mode} />
+        </Route>
+      </Switch>
     </>
+    </Router>
   );
 }
 export default App;
- 
--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 export default function About() {
     const [mystyle, setMystyle] = useState(
@@ -35,7 +36,7 @@ export default function About() {
                 <div className="card-body">
                     <h5 className="card-title">Special title treatment</h5>
                     <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                    <Link to="/" className="btn btn-primary">Go somewhere</Link>
                 </div>
                 <div className="card-footer text-muted">
                     2 days ago
